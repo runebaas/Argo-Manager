9c7053187e0db72441767f5a7eea16c2c6e446c1
sschmerek <s.schmerek@gmail.com>
2019-06-13 23:17:44 +0200

    Swagger added (#1)
    * Add default Swagger UI
    
    * Added Swagger Doc Information

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ArgoManager
 {
@@ -33,6 +34,17 @@ namespace ArgoManager
             var dockerClient = new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock")).CreateClient();
             var dockerManager = new DockerManager(dockerClient);
             services.AddSingleton<IDockerManager>(dockerManager);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { 
+                        Title = "Argo Manager", 
+                        Version = "v1",
+                        Description = "A tool to manage Argo Tunnels",
+                        Contact = new Contact { Name = "Daan Boerlage", Url = "https://github.com/runebaas" } ,
+                        License = new License { Name = "None" } 
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +59,15 @@ namespace ArgoManager
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Argo Manager V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseCors();
